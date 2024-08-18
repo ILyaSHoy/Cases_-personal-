f6e55201805bce8f9df93d45dac23e5123ff030a
ILyaSHoy <basic.holoshnoy2003@gmail.com>
2024-08-18 17:00:17 +0300

    v 1.0

--- Cases_(personal)/obj/Debug/net8.0/Cases_(personal).AssemblyInfo.cs
+++ Cases_(personal)/obj/Debug/net8.0/Cases_(personal).AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cases_(personal)")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0c6a6f102ef0ae91776efd3af76554ea49274f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b493b369e5a1542cd5eb10c3d842e49f21171eed")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cases_(personal)")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cases_(personal)")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
